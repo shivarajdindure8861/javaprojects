0d1ded9b0f13b8ce28dc2f391c6022ea7e982af9
shivaraj dindure <shivarajdindure@gmail.com>
2023-04-05 12:30:06 +0530

    yaml configuration file

--- 33_day/springBoot-understanding/src/main/java/com/shiva/boot/service/CarService.java
+++ 33_day/springBoot-understanding/src/main/java/com/shiva/boot/service/CarService.java
@@ -1,16 +1,21 @@
 package com.shiva.boot.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.shiva.boot.entity.Car;
 
 @Service
 public class CarService {
+    @Value("${ws}")
+    private String website;
+
     public void createNewCar(Long id, String make, String model) {
         Car car = new Car();
         car.setId(id);
         car.setMake(make);
         car.setModel(model);
+        System.out.println("car company website : " + website);
         System.out.println(car.toString());
     }
 }
