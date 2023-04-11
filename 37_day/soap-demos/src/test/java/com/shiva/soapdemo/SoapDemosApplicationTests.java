88a06be4d468bb4ba862529f0bde065d8cd3aba3
shivaraj dindure <shivarajdindure@gmail.com>
2023-04-11 10:43:09 +0530

    soap:intial commit

---
+++ 37_day/soap-demos/src/test/java/com/shiva/soapdemo/SoapDemosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shiva.soapdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapDemosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
