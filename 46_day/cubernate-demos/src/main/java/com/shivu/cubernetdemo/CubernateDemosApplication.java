8e89cf24f9908f8f3248b817897786687c2df61e
shivaraj dindure <shivarajdindure@gmail.com>
2023-04-25 10:38:20 +0530

    node js:funda

---
+++ 46_day/cubernate-demos/src/main/java/com/shivu/cubernetdemo/CubernateDemosApplication.java
@@ -0,0 +1,13 @@
+package com.shivu.cubernetdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CubernateDemosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CubernateDemosApplication.class, args);
+	}
+
+}
---
+++ 46_day/cubernate-demos/src/test/java/com/shivu/cubernetdemo/CubernateDemosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shivu.cubernetdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CubernateDemosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
