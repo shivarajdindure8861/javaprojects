5ee1a9af8b6c6a2ce854611f6665aac423ac33bc
shivaraj dindure <shivarajdindure@gmail.com>
2023-04-27 10:45:36 +0530

    nodeJs:es6 import and export

---
+++ 50_day/es6-modules/engine.js
@@ -0,0 +1,14 @@
+export const engine={
+    rpm:233,
+    isPistone:true,
+    make:'abc',
+    isTwoStroke:false
+}
+export const util={
+    fuelCap:7,
+    tank:45,
+    tyre:'abc'
+}
+export function calcObj(){
+    console.log('Calculating speed')
+}
---
+++ 50_day/es6-modules/fuel.js
@@ -0,0 +1,9 @@
+export default class FuelConverter{
+    constructor(){
+        this.capacity=10;
+        this.tank='metal'
+    }
+    show(){
+        console.log('capacity ${this.capacity}, Tank ${this.tank}')
+    }
+}
