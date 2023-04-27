c3c4e136687a08e950b9b18bc5598b6e418915a0
shivaraj dindure <shivarajdindure@gmail.com>
2023-04-27 10:43:57 +0530

    nodeJs:es6 modules import and export

---
+++ 50_day/modules/cars.js
@@ -0,0 +1,10 @@
+const exported=require('./engine')
+
+const{engine,util,calcObj}=exported.someObj
+
+function increaseSpeed(){
+    console.log(engine)
+    console.log(util)
+}
+increaseSpeed()
+calcObj()
